bbef3d9e082f21fbf3155b9b3cd383f9ffe0ecc0
testcodee <mp3cafe@naver.com>
2025-06-24 14:11:28 +0900

    주석

--- src/main/java/com/webStudy/jhsun/moneyBook/Controller/moneyBookController.java
+++ src/main/java/com/webStudy/jhsun/moneyBook/Controller/moneyBookController.java
@@ -1,4 +1,4 @@
-package com.webStudy.jhsun.moneyBook;
+package com.webStudy.jhsun.moneyBook.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ src/main/java/com/webStudy/jhsun/moneyBook/Mapper/moneyBookMapper.java
@@ -0,0 +1,9 @@
+package com.webStudy.jhsun.moneyBook.Mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface moneyBookMapper {
+
+
+}
