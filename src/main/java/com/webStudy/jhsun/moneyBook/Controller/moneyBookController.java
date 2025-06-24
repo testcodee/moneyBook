d8aceca1767f1eeb5313efcfe9bc462c29ed2218
testcodee <mp3cafe@naver.com>
2025-06-24 14:50:58 +0900

    커밋

--- src/main/java/com/webStudy/jhsun/moneyBook/Controller/moneyBookController.java
+++ src/main/java/com/webStudy/jhsun/moneyBook/Controller/moneyBookController.java
@@ -1,11 +1,18 @@
 package com.webStudy.jhsun.moneyBook.Controller;
 
+import com.webStudy.jhsun.moneyBook.Service.moneyBookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class moneyBookController {
+
+    private final moneyBookService moneyBookService;
+    public moneyBookController(moneyBookService moneyBookService) {
+        this.moneyBookService = moneyBookService;
+    }
+
     @GetMapping("/")
     public String showMainPage(Model model){
 
---
+++ src/main/java/com/webStudy/jhsun/moneyBook/Service/moneyBookService.java
@@ -0,0 +1,9 @@
+package com.webStudy.jhsun.moneyBook.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class moneyBookService {
+
+
+}
